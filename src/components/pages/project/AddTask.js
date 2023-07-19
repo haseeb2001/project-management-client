ef32926704801800ad8a11ac36db696ab99b425d
Muhammad Haseeb <haseebmuhammad029@gmail.com>
2023-07-19 14:47:33 +0500

    Added taskSlice and project tasks add/update form

---
+++ src/components/pages/project/AddTask.js
@@ -0,0 +1,216 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Formik } from 'formik';
+import Select from 'react-select';
+import '../../css/Sidebar.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  createTask,
+  fetchProjectTasks,
+  updateTask,
+} from '../../../store/taskSlice';
+import { taskSchema } from '../../../utils/yup/schemas';
+
+const AddTask = ({ isOpen, onClose, projectId, task, create }) => {
+  const dispatch = useDispatch();
+  const [options, setOptions] = useState([]);
+  const {
+    data: { collabs },
+  } = useSelector((state) => state.collab);
+
+  useEffect(() => {
+    if (isOpen) {
+      console.log(projectId)
+      console.log(collabs)
+      const projectMembers = collabs?.filter(
+        (collab) => collab?.projectId === projectId
+      );
+      console.log(projectMembers)
+      const userOptions = projectMembers?.map((user) => ({
+        value: user?.userId?._id,
+        label: user?.userId?.username,
+      }));
+      console.log('In useEffect', userOptions);
+      setOptions(userOptions);
+    }
+  }, [collabs, projectId, task, create, isOpen]);
+
+  const selectedUsers = task?.assigned_to?.map((assignedUserId) => {
+    const selectedUser = options?.find((user) => user.value === assignedUserId);
+    return selectedUser || null;
+  });
+
+  const initialValues = {
+    title: task?.title || '',
+    priority: task?.priority || '',
+    dueDate: task?.dueDate?.substring(0, 10) || '',
+    description: task?.description || '',
+    assigned_to: selectedUsers || [],
+  };
+
+  console.log(initialValues);
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : ''} `}>
+      <Button onClick={onClose} className='bg-white text-black border-0'>
+        Close Sidebar
+      </Button>
+
+      {options?.length > 0 && (
+        <>
+          {console.log('opts:', options, options?.length)}
+          <Formik
+            initialValues={initialValues}
+            validationSchema={taskSchema}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              const members = values?.assigned_to?.map((user) => user?.value);
+              dispatch(
+                create
+                  ? createTask({
+                      newData: {
+                        ...values,
+                        assigned_to: members,
+                        projectId: projectId,
+                      },
+                    })
+                  : updateTask({
+                      id: task?._id,
+                      newData: {
+                        ...values,
+                        assigned_to: members,
+                        projectId: projectId,
+                      },
+                    })
+              );
+
+              setSubmitting(false);
+              // resetForm();
+              onClose();
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              setFieldValue,
+              dirty,
+            }) => {
+              return (
+                <Form
+                  className='d-flex flex-column gap-3 h-full w-100'
+                  onSubmit={handleSubmit}
+                >
+                  <Form.Group className='margin-bottom-3' controlId='title'>
+                    <Form.Label className='text-white mt-2'>Title</Form.Label>
+                    <Form.Control
+                      type='text'
+                      name='title'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.title}
+                      isInvalid={touched.title && errors.title}
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                      {errors.title}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group
+                    className='margin-bottom-3'
+                    controlId='assigned_to'
+                  >
+                    <Form.Label className='text-white mt-2'>
+                      Assignee(s)
+                    </Form.Label>
+                    <Select
+                      options={options}
+                      isMulti
+                      placeholder='Collabs'
+                      value={values.assigned_to}
+                      onChange={(selectedOptions) =>
+                        setFieldValue('assigned_to', selectedOptions)
+                      }
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                      {errors.assigned_to}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className='margin-bottom-3' controlId='dueDate'>
+                    <Form.Label className='text-white mt-2'>
+                      Due Date
+                    </Form.Label>
+                    <Form.Control
+                      type='date'
+                      name='dueDate'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.dueDate}
+                      isInvalid={touched.dueDate && errors.dueDate}
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                      {errors.dueDate}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className='margin-bottom-3' controlId='priority'>
+                    <Form.Label className='text-white mt-2'>
+                      Priority
+                    </Form.Label>
+                    <Form.Control
+                      as='select'
+                      name='priority'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.priority}
+                      isInvalid={touched.priority && errors.priority}
+                    >
+                      <option>Task Priority</option>
+                      <option value='1'>1 (Low)</option>
+                      <option value='2'>2 (Low)</option>
+                      <option value='3'>3(Mild)</option>
+                      <option value='4'>4(Mild)</option>
+                      <option value='5'>5(High)</option>
+                    </Form.Control>
+                    <Form.Control.Feedback type='invalid'>
+                      {errors.priority}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group
+                    className='margin-bottom-3'
+                    controlId='description'
+                  >
+                    <Form.Label className='text-white mt-2'>
+                      Description
+                    </Form.Label>
+                    <Form.Control
+                      as='textarea'
+                      rows={5}
+                      name='description'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.description}
+                      isInvalid={touched.description && errors.description}
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                      {errors.description}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Button
+                    variant='primary'
+                    type='submit'
+                    disabled={!dirty || isSubmitting}
+                  >
+                    {isSubmitting ? 'Saving...' : 'Add Task'}
+                  </Button>
+                </Form>
+              );
+            }}
+          </Formik>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default AddTask;
---
+++ src/store/taskSlice.js
@@ -0,0 +1,157 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import { getCookie } from '../utils/cookie';
+import { STATUSES } from '../constants/statuses';
+import {
+  modifyStateHelper,
+  fetchStateHelper,
+  rejectStateHelper,
+  updateStateHelper,
+} from '../utils/reduxStateHelper';
+
+const initialState = {
+  data: [],
+  status: STATUSES.IDLE,
+  errors: [],
+};
+
+export const fetchTasks = createAsyncThunk('task/fetchTasks', async () => {
+  const token = getCookie('token');
+  const res = await fetch(`${process.env.REACT_APP_BASE_URL}/tasks/`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return await res.json();
+});
+
+export const fetchProjectTasks = createAsyncThunk(
+  'task/fetchProjectTasks',
+  async ({ projectId }) => {
+    const token = getCookie('token');
+    const res = await fetch(
+      `${process.env.REACT_APP_BASE_URL}/tasks/project/${projectId}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return await res.json();
+  }
+);
+
+export const createTask = createAsyncThunk(
+  'task/createTask',
+  async ({ newData }) => {
+    console.log(newData);
+    const token = getCookie('token');
+    const res = await fetch(`${process.env.REACT_APP_BASE_URL}/tasks/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(newData),
+    });
+
+    return await res.json();
+  }
+);
+
+export const updateTask = createAsyncThunk(
+  'task/updateTask',
+  async ({ id, newData }) => {
+
+    console.log('in redux', newData)
+    const token = getCookie('token');
+    const res = await fetch(`${process.env.REACT_APP_BASE_URL}/tasks/${id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(newData),
+    });
+    const data = await res.json()
+
+    console.log(data)
+    return data;
+  }
+);
+
+export const deleteTask = createAsyncThunk(
+  'task/deleteTask',
+  async ({ id }) => {
+    const token = getCookie('token');
+    const res = await fetch(`${process.env.REACT_APP_BASE_URL}/tasks/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return await res.json();
+  }
+);
+
+const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTasks.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        return fetchStateHelper(state, action.payload, 'tasks');
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        rejectStateHelper(state);
+      })
+      .addCase(fetchProjectTasks.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchProjectTasks.fulfilled, (state, action) => {
+        return fetchStateHelper(state, action.payload, 'tasks');
+      })
+      .addCase(fetchProjectTasks.rejected, (state, action) => {
+        rejectStateHelper(state);
+      })
+      .addCase(createTask.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(createTask.fulfilled, (state, action) => {
+        return updateStateHelper(state, action.payload, 'task', 'tasks');
+      })
+      .addCase(createTask.rejected, (state, action) => {
+        rejectStateHelper(state);
+      })
+      .addCase(updateTask.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(updateTask.fulfilled, (state, action) => {
+        return updateStateHelper(state, action.payload, 'task', 'tasks')
+      })
+      .addCase(updateTask.rejected, (state, action) => {
+        rejectStateHelper(state);
+      })
+      .addCase(deleteTask.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(deleteTask.fulfilled, (state, action) => {
+        modifyStateHelper(state, action.payload);
+      })
+      .addCase(deleteTask.rejected, (state, action) => {
+        rejectStateHelper(state);
+      });
+  },
+});
+
+export default taskSlice.reducer;
